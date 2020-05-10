3922802c18149da723dcb7c0e0c6614442e06ec0
d-tsuji <dram.dt.shonan@gmail.com>
2020-05-10 09:44:47 +0900

    update test case

--- clipboard_test.go
+++ clipboard_test.go
@@ -1,21 +1,28 @@
 package clipboard
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestCopyAndPaste(t *testing.T) {
-	want := "gopher"
-	if err := Set(want); err != nil {
-		t.Error(err)
+	tests := []struct {
+		name string
+		want string
+	}{
+		{"basic", "gopher"},
+		{"Japanese", "ゴーファー"},
+		{"emoji", "😀😁😂🤣😃😄😅"},
 	}
-
-	got, err := Get()
-	if err != nil {
-		t.Error(err)
-	}
-
-	if got != want {
-		t.Errorf("CopyAndPaste mismatch: got %v, want %v", got, want)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Set(tt.want); err != nil {
+				t.Error(err)
+			}
+			got, err := Get()
+			if err != nil {
+				t.Error(err)
+			}
+			if got != tt.want {
+				t.Errorf("CopyAndPaste mismatch: got %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
